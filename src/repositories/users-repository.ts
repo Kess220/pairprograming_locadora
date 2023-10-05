76e7a59eaed23e63c55fa1262ae15c5ecace2dbe
Eduardo André <eduardo_andre@id.uff.br>
2023-10-05 15:11:43 -0300

    feat : testes unitarios

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,31 +1,32 @@
 import prisma from "../database";
 
 async function getById(id: number) {
-  return await prisma.user.findUnique({
-    where: { id },
-  });
+	return await prisma.user.findUnique({
+		where: { id },
+	});
 }
 
 export async function createUser(
-  firstName: string,
-  lastName: string,
-  email: string,
-  cpf: string,
-  birthDate: Date
+	firstName: string,
+	lastName: string,
+	email: string,
+	cpf: string,
+	birthDate: Date
 ) {
-  const user = await prisma.user.create({
-    data: {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      cpf: cpf,
-      birthDate,
-    },
-  });
+	const user = await prisma.user.create({
+		data: {
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			cpf: cpf,
+			birthDate,
+		},
+	});
 
-  return user;
+	return user;
 }
 
 export default {
-  getById,
+	getById,
+	createUser
 };
--- tests/unit/rentals-unit.test.ts
+++ tests/unit/rentals-unit.test.ts
@@ -1,114 +1,182 @@
 import repository from "../../src/repositories/rentals-repository";
 import prisma from "../../src/database";
 import { cleanDb } from "../utils";
+import moviesRepository from "repositories/movies-repository";
+import { RentalInput } from "protocols";
+import rentalsService from "services/rentals-service";
 beforeEach(async () => await cleanDb());
 describe("Rentals Service Unit Tests", () => {
-  it("should do something", async () => {
-    const movies = await prisma.movie.createMany({
-      data: [
-        { id: 1, adultsOnly: false, name: "Movie 1" },
-        { id: 2, adultsOnly: false, name: "Movie 2" },
-        { id: 3, adultsOnly: false, name: "Movie 3" },
-        { id: 4, adultsOnly: false, name: "Movie 4" },
-        { id: 5, adultsOnly: false, name: "Movie 5" },
-      ],
-    });
+	it("should do something", async () => {
+		const movies = await prisma.movie.createMany({
+			data: [
+				{ id: 1, adultsOnly: false, name: "Movie 1" },
+				{ id: 2, adultsOnly: false, name: "Movie 2" },
+				{ id: 3, adultsOnly: false, name: "Movie 3" },
+				{ id: 4, adultsOnly: false, name: "Movie 4" },
+				{ id: 5, adultsOnly: false, name: "Movie 5" },
+			],
+		});
 
-    const user = await prisma.user.create({
-      data: {
-        firstName: "CNPJOTO",
-        lastName: "ARARAQUARO",
-        birthDate: new Date(1998, 6, 12),
-        cpf: "00000000000",
-        email: "cnpjoto.araraquaro@gmail.com",
-      },
-    });
+		const user = await prisma.user.create({
+			data: {
+				firstName: "CNPJOTO",
+				lastName: "ARARAQUARO",
+				birthDate: new Date(1998, 6, 12),
+				cpf: "00000000000",
+				email: "cnpjoto.araraquaro@gmail.com",
+			},
+		});
 
-    const spyCreaterental = jest.spyOn(repository, "createRental");
+		const spyCreaterental = jest.spyOn(repository, "createRental");
 
-    await repository.createRental({ moviesId: [1], userId: user.id });
+		await repository.createRental({ moviesId: [1], userId: user.id });
 
-    const rentals = await repository.getRentalsByUserId(user.id, false);
+		const rentals = await repository.getRentalsByUserId(user.id, false);
 
-    expect(rentals[0].id).toBeGreaterThan(0);
-  });
-  it("should not create a rental with more than 4 movies", async () => {
-    const movies = await prisma.movie.createMany({
-      data: [
-        { id: 1, adultsOnly: false, name: "Movie 1" },
-        { id: 2, adultsOnly: false, name: "Movie 2" },
-        { id: 3, adultsOnly: false, name: "Movie 3" },
-        { id: 4, adultsOnly: false, name: "Movie 4" },
-        { id: 5, adultsOnly: false, name: "Movie 5" },
-      ],
-    });
+		expect(rentals[0].id).toBeGreaterThan(0);
+	});
+	it("should not create a rental with more than 4 movies", async () => {
+		const movies = await prisma.movie.createMany({
+			data: [
+				{ id: 1, adultsOnly: false, name: "Movie 1" },
+				{ id: 2, adultsOnly: false, name: "Movie 2" },
+				{ id: 3, adultsOnly: false, name: "Movie 3" },
+				{ id: 4, adultsOnly: false, name: "Movie 4" },
+				{ id: 5, adultsOnly: false, name: "Movie 5" },
+			],
+		});
 
-    const user = await prisma.user.create({
-      data: {
-        firstName: "CNPJOTO",
-        lastName: "ARARAQUARO",
-        birthDate: new Date(1998, 6, 12),
-        cpf: "00000000000",
-        email: "cnpjoto.araraquaro@gmail.com",
-      },
-    });
+		const user = await prisma.user.create({
+			data: {
+				firstName: "CNPJOTO",
+				lastName: "ARARAQUARO",
+				birthDate: new Date(1998, 6, 12),
+				cpf: "00000000000",
+				email: "cnpjoto.araraquaro@gmail.com",
+			},
+		});
 
-    try {
-      // Tente criar um aluguel com mais de 4 filmes
-      await repository.createRental({
-        moviesId: [1, 2, 3, 4, 5], // Escolha cinco filmes
-        userId: user.id,
-      });
+		try {
+			// Tente criar um aluguel com mais de 4 filmes
+			await repository.createRental({
+				moviesId: [1, 2, 3, 4, 5], // Escolha cinco filmes
+				userId: user.id,
+			});
 
-      // Se a função não lançar um erro, o teste deve falhar
-    } catch (error) {
-      // Verifique se o erro corresponde à mensagem esperada
-      expect(error.message).toBe(
-        "At least 1 movie and at most 4 movies are required for rental"
-      );
-    }
-  });
+			// Se a função não lançar um erro, o teste deve falhar
+		} catch (error) {
+			// Verifique se o erro corresponde à mensagem esperada
+			expect(error.message).toBe(
+				"At least 1 movie and at most 4 movies are required for rental"
+			);
+		}
+	});
 
-  it("should not allow a user to rent if they have a pending rental", async () => {
-    // Criar filmes, se necessário
-    const movies = await prisma.movie.createMany({
-      data: [
-        { id: 1, adultsOnly: false, name: "Movie 1" },
-        { id: 2, adultsOnly: false, name: "Movie 2" },
-        { id: 3, adultsOnly: false, name: "Movie 3" },
-        { id: 4, adultsOnly: false, name: "Movie 4" },
-        { id: 5, adultsOnly: false, name: "Movie 5" },
-      ],
-    });
+	it("should not allow a user to rent if they have a pending rental", async () => {
+		// Criar filmes, se necessário
+		const movies = await prisma.movie.createMany({
+			data: [
+				{ id: 1, adultsOnly: false, name: "Movie 1" },
+				{ id: 2, adultsOnly: false, name: "Movie 2" },
+				{ id: 3, adultsOnly: false, name: "Movie 3" },
+				{ id: 4, adultsOnly: false, name: "Movie 4" },
+				{ id: 5, adultsOnly: false, name: "Movie 5" },
+			],
+		});
 
-    // Criar um usuário
-    const user = await prisma.user.create({
-      data: {
-        firstName: "CNPJOTO",
-        lastName: "ARARAQUARO",
-        birthDate: new Date(1998, 6, 12),
-        cpf: "00000000000",
-        email: "cnpjoto.araraquaro@gmail.com",
-      },
-    });
+		// Criar um usuário
+		const user = await prisma.user.create({
+			data: {
+				firstName: "CNPJOTO",
+				lastName: "ARARAQUARO",
+				birthDate: new Date(1998, 6, 12),
+				cpf: "00000000000",
+				email: "cnpjoto.araraquaro@gmail.com",
+			},
+		});
 
-    // Criar uma locação pendente para o usuário
-    await repository.createRental({
-      moviesId: [1, 2], // IDs dos filmes para a locação pendente
-      userId: user.id,
-    });
+		// Criar uma locação pendente para o usuário
+		await repository.createRental({
+			moviesId: [1, 2], // IDs dos filmes para a locação pendente
+			userId: user.id,
+		});
 
-    try {
-      // Tentar criar um novo aluguel sem a propriedade 'closed'
-      await repository.createRental({
-        moviesId: [3, 4], // IDs dos filmes para a nova locação
-        userId: user.id,
-      });
+		try {
+			// Tentar criar um novo aluguel sem a propriedade 'closed'
+			await repository.createRental({
+				moviesId: [3, 4], // IDs dos filmes para a nova locação
+				userId: user.id,
+			});
 
-      // Se a função não lançar um erro, o teste deve falhar
-    } catch (error) {
-      // Verifique se o erro corresponde à mensagem esperada
-      expect(error.message).toBe("The user already has a rental!");
-    }
-  });
+			// Se a função não lançar um erro, o teste deve falhar
+		} catch (error) {
+			// Verifique se o erro corresponde à mensagem esperada
+			expect(error.message).toBe("The user already has a rental!");
+		}
+	});
+	it("should return 'Cannot see that movie.' if uuser is not old enough", async() => 
+	{
+		const 
+			 user ={
+				id: 0,
+				firstName: 'string',
+				lastName: 'string',
+				email: 'string',
+				cpf: 'string',
+				birthDate: new Date()
+			}
+		jest.spyOn(moviesRepository,"getById").mockImplementation(():any => 
+		{
+			const movie = 
+			{
+				id: 69,
+				adultsOnly:true
+			}
+			return movie
+		})
+		const rentalInput:RentalInput = 
+		{
+			moviesId : [69],
+			userId : 69
+		}
+		const createRental = rentalsService.checkMoviesValidForRental([69],user)
+		expect(createRental).rejects.toEqual(
+			{
+				name:'InsufficientAgeError',
+				message:"Cannot see that movie."
+			})
+	})
+	it("should return an error if rentalId !== null", async () => 
+	{
+		const 
+			 user ={
+				id: 0,
+				firstName: 'string',
+				lastName: 'string',
+				email: 'string',
+				cpf: 'string',
+				birthDate: new Date()
+			}
+		jest.spyOn(moviesRepository,"getById").mockImplementation(():any => 
+		{
+			const movie = 
+			{
+				id: 69,
+				adultsOnly:false,
+				rentalId: 4
+			}
+			return movie
+		})
+		const rentalInput:RentalInput = 
+		{
+			moviesId : [69],
+			userId : 69
+		}
+		const createRental = rentalsService.checkMoviesValidForRental([69],user)
+		expect(createRental).rejects.toEqual(
+			{
+				name:'MovieInRentalError',
+				message:"Movie already in a rental."
+			})
+	})
 });
